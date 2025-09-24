7760d57d3fb404823f008360a58ccb89bed3fef5
Lucas <lucasbo.dev@gmail.com>
2025-09-24 18:31:09 -0300

    feat: Implement FilmeService with business logic

---
+++ src/main/java/br/com/pipocaIA/service/FilmeService.java
@@ -0,0 +1,73 @@
+package br.com.pipocaIA.service;
+
+import br.com.pipocaIA.dto.FilmeDTO;
+import br.com.pipocaIA.dto.mapper.FilmeMapper;
+import br.com.pipocaIA.enums.Categoria;
+import br.com.pipocaIA.model.FilmeModel;
+import br.com.pipocaIA.repository.FilmeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class FilmeService {
+
+    private FilmeRepository filmeRepository;
+    private FilmeMapper filmeMapper;
+
+    public FilmeService(FilmeRepository filmeRepository, FilmeMapper filmeMapper) {
+        this.filmeRepository = filmeRepository;
+        this.filmeMapper = filmeMapper;
+    }
+
+
+    public List<FilmeDTO> filmesRegistrados(){
+        List<FilmeModel> filmeModelList = filmeRepository.findAll();
+        return filmeModelList.stream()
+                .map(filmeMapper::map)
+                .collect(Collectors.toList());
+    }
+
+    public List<FilmeDTO> listarFilmesCategoria(Categoria categoria){
+        List<FilmeModel> filmeModelCategoriaList = filmeRepository.findAllByCategoria(categoria);
+        return filmeModelCategoriaList.stream()
+                .map(filmeMapper::map)
+                .collect(Collectors.toList());
+    }
+
+    public FilmeDTO listarFilmeId(Long id){
+        Optional<FilmeModel> filmeModelProcurado = filmeRepository.findById(id);
+
+        if (filmeModelProcurado.isPresent()){
+            FilmeModel filmeModelEncontrado = filmeModelProcurado.get();
+            return filmeMapper.map(filmeModelEncontrado);
+        } return null;
+    }
+
+    public FilmeDTO registrarFilme(FilmeDTO filmeDTO){
+        FilmeModel filmeModelNovo = filmeRepository.save(filmeMapper.map(filmeDTO));
+        return filmeMapper.map(filmeModelNovo);
+    }
+
+    public boolean deletarFilme(Long id){
+        Optional<FilmeModel> filmeModelProcurado = filmeRepository.findById(id);
+
+        if(filmeModelProcurado.isPresent()){
+            filmeRepository.deleteById(id);
+            return true;
+        } return false;
+    }
+
+    public FilmeDTO alterarFilme(Long id, FilmeDTO filmeDTO){
+        Optional<FilmeModel> filmeModelProcurado = filmeRepository.findById(id);
+
+        if(filmeModelProcurado.isPresent()){
+            FilmeModel filmeModelAtualizado = filmeMapper.map(filmeDTO);
+            filmeModelAtualizado.setId(id);
+            return filmeMapper.map(filmeRepository.save(filmeModelAtualizado));
+        } return null;
+    }
+
+}
