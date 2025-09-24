4f0c5712f0976c0ff09fb9362137a1daec6c9796
Lucas <lucasbo.dev@gmail.com>
2025-09-24 18:30:52 -0300

    feat: Define Categoria enum for movie genres

---
+++ src/main/java/br/com/pipocaIA/enums/Categoria.java
@@ -0,0 +1,14 @@
+package br.com.pipocaIA.enums;
+
+public enum Categoria {
+    ACAO,
+    AVENTURA,
+    COMEDIA,
+    DRAMA,
+    FICCAO,
+    TERROR,
+    SUSPENSE,
+    ROMANCE,
+    ANIMACAO,
+    DOCUMENTARIO
+}
