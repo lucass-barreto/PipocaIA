620eabf036b23fa7e6824a8e109dea63fd89853a
Lucas <lucasbo.dev@gmail.com>
2025-09-24 17:03:24 -0300

    feat: Initial project setup

---
+++ src/main/java/br/com/pipocaIA/PipocaIaApplication.java
@@ -0,0 +1,13 @@
+package br.com.pipocaIA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipocaIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipocaIaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pipocaIA/PipocaIaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pipocaIA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipocaIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
