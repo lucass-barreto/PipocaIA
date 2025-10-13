9c36b36b23fb49dc61a7ae6dfbb75dee15161c18
Lucas <lucasbo.dev@gmail.com>
2025-10-13 12:49:56 -0300

    style(controllerUI): remove unnecessary blank line
    Removed an extra blank line in `ControllerUI.java` to improve code readability and maintain consistent formatting.

--- src/main/java/br/com/pipocaIA/controller/ControllerUI.java
+++ src/main/java/br/com/pipocaIA/controller/ControllerUI.java
@@ -8,6 +8,7 @@ import br.com.pipocaIA.service.TmdbFilmeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @Controller
